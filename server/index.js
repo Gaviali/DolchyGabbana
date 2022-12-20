166516038b6eddfdcc4b5507f528cc2861c15257
Jeffrey Zhang <jzhang22la@gmail.com>
2022-12-19 17:37:40 -0800

    started adding things to index already, about to slow down and go back to whiteboarding, just wanted to be sure to set up

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 require('dotenv').config();
 const path = require('path');
+const { Client } = require('pg');
+const client = new Client({
+  host: '',
+  port: 3000,
+  user: 'root',
+  password: '',
+});
+
+client
+  .connect()
+  .then(() => console.log('connected postgreSQL'))
+  .catch((err) => console.error('connection error: ', err.stack));
 
 const express = require('express');
 const app = express();
@@ -7,6 +19,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+client.query('SELECT $1::text as message', ['HELLO WORLD!!!'], (err, res) => {
+  console.log(err ? err.stack : res.rows[0].message);
+  client.end();
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
