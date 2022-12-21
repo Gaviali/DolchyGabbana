f5b87904198133ee82286f417b1a68277704b94f
Jeffrey Zhang <jzhang22la@gmail.com>
2022-12-21 10:53:58 -0800

    commented out for now

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 const path = require('path');
-const { Client } = require('pg');
-const client = new Client({
-  host: '',
-  port: 3000,
-  user: 'root',
-  password: '',
-});
+// const { Client } = require('pg');
+// const client = new Client(
+//   {
+//   host: 'localhost',
+//   port: 3000,
+//   user: 'root',
+//   password: '',
+// }
+// );
 
-client
-  .connect()
-  .then(() => console.log('connected postgreSQL'))
-  .catch((err) => console.error('connection error: ', err.stack));
+// client
+//   .connect()
+//   .then(() => console.log('connected postgreSQL'))
+//   .catch((err) => console.error('connection error: ', err.stack));
 
 const express = require('express');
 const app = express();
@@ -20,10 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-client.query('SELECT $1::text as message', ['HELLO WORLD!!!'], (err, res) => {
-  console.log(err ? err.stack : res.rows[0].message);
-  client.end();
-});
+// client.query('SELECT $1::text as message', ['HELLO WORLD!!!'], (err, res) => {
+//   console.log(err ? err.stack : res.rows[0].message);
+//   client.end();
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
