8b25e725d6dce5f85e9da5f72f0902d1473d2431
Jeffrey Zhang <jzhang22la@gmail.com>
2022-12-22 18:15:46 -0800

    trying to figure out what pool should be hosting on

--- databases/reviewspg.js
+++ databases/reviewspg.js
@@ -1,14 +1,19 @@
-const { Pool, Client } = require('pg');
+const { Pool } = require('pg');
 
 const pool = new Pool({
+  user: '',
   host: 'localhost',
-  user: 'root',
-  password: '',
   database: 'Reviews',
+  password: '',
   port: 5432,
 });
 
-pool
-  .connect()
-  .then(() => console.log('connected postgreSQL'))
-  .catch((err) => console.error('Connection error: ', err));
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+
+// pool
+//   .connect()
+//   .then(() => console.log('connected postgreSQL'))
+//   .catch((err) => console.error('Connection error: ', err));
