5038be4263b0892ec24d7c13e64f7360deda2256
Jeffrey Zhang <jzhang22la@gmail.com>
2022-12-22 18:08:29 -0800

    commented out extra table, formatted page a little and setup for using csv files

--- databases/reviewspg.sql
+++ databases/reviewspg.sql
@@ -6,20 +6,22 @@ CREATE DATABASE Reviews;
 \c Reviews;
 
 DROP TABLE IF EXISTS  Reviews CASCADE;
-DROP TABLE IF EXISTS  Results CASCADE;
+-- DROP TABLE IF EXISTS  Results CASCADE;
 DROP TABLE IF EXISTS  ReviewsPhotos CASCADE;
 DROP TABLE IF EXISTS  Characteristics CASCADE;
 DROP TABLE IF EXISTS  CharReview CASCADE;
 
--- Table 'Reviews'
-CREATE TABLE  Reviews  (
-   product_id  SERIAL PRIMARY KEY,
-   page  INTEGER ,
-   count  INTEGER ,
-);
+-- Table 'Reviews' no csv file for with page or count
 
--- Table 'Results'
-CREATE TABLE Results (
+-- CREATE TABLE  Reviews  (
+--    product_id  SERIAL PRIMARY KEY,
+--    page  INT ,
+--    count  INT ,
+-- );
+
+-- Table 'Reviews'
+
+CREATE TABLE Reviews (
    review_id  SERIAL PRIMARY KEY,
    product_id  SERIAL ,
    rating  INT ,
@@ -34,22 +36,30 @@ CREATE TABLE Results (
    helpfulness  INT ,
 );
 
+-- load csv data onto table
 
 -- Table 'Reviews Photos'
+
 CREATE TABLE  ReviewsPhotos  (
    photo_id  SERIAL PRIMARY KEY,
    review_id  SERIAL,
    url  TEXT ,
 );
 
+-- load csv data onto table
+
 -- Table 'Characteristics'
+
 CREATE TABLE  Characteristics  (
   char_id SERIAL PRIMARY KEY,
   product_id SERIAL,
   name TEXT,
 );
 
+-- load csv data onto table
+
 -- Table 'Characteristics'
+
 CREATE TABLE  CharReview  (
   charReview_id SERIAL PRIMARY KEY,
   chars_id SERIAL,
@@ -57,10 +67,11 @@ CREATE TABLE  CharReview  (
   value INT,
 );
 
+-- load csv data onto table
+
 -- Foreign Keys
 
-ALTER TABLE  Results  ADD FOREIGN KEY (product_id) REFERENCES  Reviews  ( product_id );
-ALTER TABLE  ReviewPhotos  ADD FOREIGN KEY (review_id) REFERENCES  Results  ( review_id );
+ALTER TABLE  ReviewPhotos  ADD FOREIGN KEY (review_id) REFERENCES  Reviews  ( review_id );
 ALTER TABLE  Characteristics  ADD FOREIGN KEY (product_id) REFERENCES  Reviews  ( product_id );
 ALTER TABLE  CharReview ADD FOREIGN KEY (chars_id) REFERENCES Characteristics  ( char_id );
 ALTER TABLE  CharReview ADD FOREIGN KEY (review_id) REFERENCES Reviews  ( review_id );
